4c07ba21319250113748087765e4360968bee5df
Jonathan Weiss <jw@innerewut.de>
2010-09-30 13:43:32 +0200

    Handle auto-boxing correctly when RockingChair is disabled

--- lib/rocking_chair/http_adapter.rb
+++ lib/rocking_chair/http_adapter.rb
@@ -16,46 +16,46 @@ module RockingChair
   
     def get(uri, headers=nil)
       result = http_adapter.get(uri)
-      if result.is_a?(Hash)
-        result
-      else
+      if result.is_a?(String)
         JSON.parse(result)
+      else
+        result
       end
     end
   
     def post(uri, payload, headers=nil)
       result = http_adapter.post(uri, payload)
-      if result.is_a?(Hash)
-        result
-      else
+      if result.is_a?(String)
         JSON.parse(result)
+      else
+        result
       end
     end
   
     def put(uri, payload=nil, headers=nil)
       result = http_adapter.put(uri, payload)
-      if result.is_a?(Hash)
-        result
-      else
+      if result.is_a?(String)
         JSON.parse(result)
+      else
+        result
       end
     end
   
     def delete(uri, headers=nil)
       result = http_adapter.delete(uri)
-      if result.is_a?(Hash)
-        result
-      else
+      if result.is_a?(String)
         JSON.parse(result)
+      else
+        result
       end
     end
   
     def copy(uri, destination)
       result = http_adapter.copy(uri, default_headers.merge('Destination' => destination))
-      if result.is_a?(Hash)
-        result
-      else
+      if result.is_a?(String)
         JSON.parse(result)
+      else
+        result
       end
     end    
  
